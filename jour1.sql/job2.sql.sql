719f5bb8114643beab0266df0df3185fa2e29e5d
Siham-ed-dhimine <130568831+Siham-EDDHIMINE@users.noreply.github.com>
2023-05-06 08:47:04 +0200

    Add files via upload

---
+++ jour1.sql/job2.sql.sql
@@ -0,0 +1,7 @@
+CREATE TABLE etudiants (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nom VARCHAR(255) NOT NULL,
+    prenom VARCHAR(25) NOT NULL,
+    age INT NOT NULL,
+    email VARCHAR(255) NOT NULL
+);
